248b5d7b479cea7f0a19dec8cf10fe9bbcba8c2a
Ravi Vaghela <rvaghela@fleetoperate.com>
2024-10-18 14:54:40 +0530

    build:  installed mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CommentsModule } from './comments/comments.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, CommentsModule],
+  imports: [
+    UsersModule,
+    CommentsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://rvdev17:j7m5FQMlzg7zqOfy@cluster0.mw8uhzl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
